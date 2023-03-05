8e92576cdb00918601b69d61f1cdff0a47f3c53d
Лёха <Alexbac@yandex.ru>
2023-03-05 20:22:03 +0300

    Генератор массива

---
+++ Example011/Program.cs
@@ -0,0 +1,28 @@
+﻿void fillaray(int[] collection)
+{
+
+int length = collection.Length;
+int index = 0;
+while (index < length)
+{
+    collection[index] = new Random().Next(1,100);
+    index++;
+}
+}
+
+void writearay(int[] col)
+{
+    int count = col.Length;
+    int positon = 0;
+    while (positon < count)
+    {
+        Console.Write(col[positon]);
+        Console.Write(", ");
+        positon++;
+    }
+}
+
+//int [] aray = new int[5];
+int [] aray = {1, 2, 3, 4, 5, 6, 7, 8};
+//fillaray(aray);
+writearay(aray);
