116dd5a4bb8263fc606eb4f2917b4984dd168d54
Лёха <Alexbac@yandex.ru>
2023-03-05 19:30:49 +0300

    Поиск индекса числа в массиве

---
+++ Example010/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] aray = {1, 2, 3,100, 4, 5, 6, 7,100};
+
+int index = 0;
+int n = aray.Length;
+
+int find = 300;
+
+while (index < n)
+{
+    if (aray[index] == find)
+    {
+        Console.Write("Индекс искомого числа в массиве соответствует: -  ");
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+    
+}
