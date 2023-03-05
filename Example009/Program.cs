cb780032c700a62a9ae6a44e1421095a16c330c5
Лёха <Alexbac@yandex.ru>
2023-03-05 18:51:08 +0300

    Длбавил массив

---
+++ Example009/Program.cs
@@ -0,0 +1,15 @@
+﻿int arg (int arg1, int arg2, int arg3)
+{
+    int max = arg1;
+    if (arg2 > max) max = arg2;
+    if (arg3 > max) max = arg3;
+    return max;
+}
+
+int [] aray = { 11, 12222, 13, 14, 15, 600, 71, 81, 91};
+
+int Max = arg(arg(aray[0], aray[1], aray[2]),
+              arg(aray[3], aray[4], aray[5]),
+              arg(aray[6], aray[7], aray[8]));
+
+Console.WriteLine(Max);
