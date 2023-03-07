035330a7a1c58b5498d9e379f756921a872b240c
Лёха <Alexbac@yandex.ru>
2023-03-07 23:42:12 +0300

    Десятичный в бинарный код

---
+++ 10 in 2/Program.cs
@@ -0,0 +1,16 @@
+﻿void fillbin(int x)
+{
+    int index = 0;
+    while( x != 0)
+    {
+        int rez = (x % 2);
+        x = x/2;
+        Console.Write(rez);
+        index++;
+    }
+} 
+
+
+Console.Write("Введите десятичное число: ");
+int des = int.Parse(Console.ReadLine());
+fillbin(des);
